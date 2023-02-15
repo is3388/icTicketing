6bf73eec942c9d7d64de4c57cb178afdaf8159a9
is3388 <meiychin@yahoo.com>
2023-02-15 18:35:22 -0500

    Added comment to change baseURL to real domain name for PROD cluster

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -10,6 +10,7 @@ const buildClient = ({ req }) => {
     // we are on the server,  we are running our app or rendering our app on Kubernetes cluster during the 
     // server side rendering phase
     // axios.create() to create an instance of axios
+    // baseURL now is for DEV cluster, need to put down the real domain name as baseURL for PROD cluster in Digital Ocean
     return axios.create({
       baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers
